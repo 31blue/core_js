c017193affbd052a1b6c0f965e2ed5aa39a8eed6
kindtiger <seonbeom2@gmail.com>
2024-11-27 15:50:46 +0900

    learn. figma to web component
    - Counter

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@
 
 import { Button } from "./components/Button/Button.js";
 import { Card } from "./components/Card/Card.js";
+import { Counter } from "./components/Counter/Counter.js";
 
 
 
@@ -14,6 +15,7 @@ const app = document.getElementById('app');
 function defineElements(){
   customElements.define('custom-button',Button)
   customElements.define('custom-card',Card)
+  customElements.define('custom-counter',Counter)
 
 
 }
