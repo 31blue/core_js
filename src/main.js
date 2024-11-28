56c3dc8a7095d458520cd9f9940bb4210f12e231
kindtiger <seonbeom2@gmail.com>
2024-11-28 15:50:54 +0900

    learn. vite
    - vite.config.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 
 
+import '@/style/style.css';
 import { getNode, insertLast } from "kind-tiger";
-import santa from "/src/assets/santa.png";
+import santa from "@/assets/santa.png";
+import {btn} from "@/style/style.module.css";
+
+
 
 const app = getNode('#app');
 
@@ -11,6 +15,7 @@ const template = /* html */`
     <img style="width:30vw" src="${santa}"/>
     <figcaption>산타 모자를 쓴 호랑이</figcaption>
   </figure>
+  <button type="button" class="${btn}">BUTTON</button>
 `
 
 
